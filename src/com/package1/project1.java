cd2fe76b6604240931b2c4a9c79aea7ff97ebaea
Wediah <100675006+Wediah@users.noreply.github.com>
2023-02-23 19:52:39 +0000

    adds methods

--- src/com/package1/project1.java
+++ src/com/package1/project1.java
@@ -2,10 +2,11 @@ package com.package1;
 import java.util.Scanner;
 
 public class project1 {
+
+    static void myMethod() {
+        System.out.println("I just got executed!");
+    }
     public static void main(String[] args) {
-        String[] cars = {"Volvo", "BMW", "Ford", "Mazda", "Benz"};
-        for (int i = 0 ; i < cars.length; i++){
-            System.out.println(cars[i]);
-        }
+        myMethod();
     }
 }
