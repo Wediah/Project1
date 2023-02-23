3c0cb80856e18a5cd3acc36ed17fa963cff107df
Wediah <100675006+Wediah@users.noreply.github.com>
2023-02-23 19:44:47 +0000

    adds benz

---
+++ src/com/package1/project1.java
@@ -0,0 +1,11 @@
+package com.package1;
+import java.util.Scanner;
+
+public class project1 {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda", "Benz"};
+        for (int i = 0 ; i < cars.length; i++){
+            System.out.println(cars[i]);
+        }
+    }
+}
