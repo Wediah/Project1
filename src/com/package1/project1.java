2b3799aa5dfb567b157ccd329b0bd37c0b94c685
Wediah <100675006+Wediah@users.noreply.github.com>
2023-02-24 10:43:25 +0000

    overloaded methods

--- src/com/package1/project1.java
+++ src/com/package1/project1.java
@@ -2,11 +2,18 @@ package com.package1;
 import java.util.Scanner;
 
 public class project1 {
-
-    static void myMethod() {
-        System.out.println("I just got executed!");
+    static int plusMethod(int x, int y){
+        return x + y;
     }
+
+    static double plusMethod(double x, double y){
+        return x + y;
+    }
+
     public static void main(String[] args) {
-        myMethod();
+        int myNum1 = plusMethod(8, 5);
+        double myNum2 = plusMethod(4.3, 6.26);
+        System.out.println("int:  "+ myNum1);
+        System.out.println("double:  "+ myNum2);
     }
 }
